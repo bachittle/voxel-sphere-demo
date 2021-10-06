fb1426f7ab99f82a75b5c886cf7c4ebce9fbd7c8
Bailey Chittle <bachittle@gmail.com>
2021-10-05 20:38:50 -0400

    initial three setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
+import * as THREE from 'three';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
